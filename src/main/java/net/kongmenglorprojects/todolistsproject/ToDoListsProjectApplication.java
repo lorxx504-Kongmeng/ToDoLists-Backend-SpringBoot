3b366ff2fb7a82673fd8cd528d3f2417ed65f2f5
kongmenglor <lorxx504@umn.edu>
2023-01-05 00:08:06 -0600

    init

--- src/main/java/net/kongmenglorprojects/todolistsproject/ToDoListsProjectApplication.java
+++ src/main/java/net/kongmenglorprojects/todolistsproject/ToDoListsProjectApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ToDoListsProjectApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ToDoListsProjectApplication.class, args);
 	}
